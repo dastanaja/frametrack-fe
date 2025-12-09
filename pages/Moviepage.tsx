9761893f97cd524a59a151ba8afef6ac51fb4558
dastanaja <garnidadastan20@gmail.com>
2025-12-10 00:38:35 +0700

    add animation

--- pages/Moviepage.tsx
+++ pages/Moviepage.tsx
@@ -6,6 +6,7 @@ import { useParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Rating from '@/components/Rating'
+import { motion } from 'framer-motion'
 
 function Moviepage() {
     const param = useParams()
@@ -34,19 +35,32 @@ function Moviepage() {
       >
         {
           movie.primaryImage && (
-            <div className='absolute right-0 left-1/5 top-0 w-full h-full'>
+            <motion.div 
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:0.5, ease:'easeOut'}}
+            className='absolute right-0 left-1/5 top-0 w-full h-full'>
               <div className='absolute inset-x-0 top-0 w-1/4 h-full bg-linear-to-r from-background to-transparent'></div>
               <div className='absolute inset-x-0 bottom-0 w-full h-1/4 bg-linear-to-t from-background to-transparent'></div>
               <Image className='w-4/5' src={movie.primaryImage.url} alt='Movie Banner' width={movie.primaryImage.width} height={movie.primaryImage.height} />
-            </div>
+            </motion.div>
           )
         }
 
         <div className='relative flex flex-col w-2/3 z-10 pb-6'>
-        <Rating rating={movie.rating.aggregateRating}/>
-          <h1 className='text-5xl font-medium'>{movie.primaryTitle}</h1>
+        <motion.div initial={{opacity:0, translateY:20}}
+            animate={{opacity:1, translateY:0}}
+            transition={{duration:0.5, ease:'easeOut', delay:0.1}}><Rating rating={movie.rating.aggregateRating}/></motion.div>
+          <motion.h1 
+          initial={{opacity:0, translateY:20}}
+            animate={{opacity:1, translateY:0}}
+            transition={{duration:0.5, ease:'easeOut', delay:0.2}}
+          className='text-5xl font-medium'>{movie.primaryTitle}</motion.h1>
           <div className='flex flex-col gap-1 mt-4'>
-            <p>
+            <motion.p
+            initial={{opacity:0, translateY:20}}
+            animate={{opacity:1, translateY:0}}
+            transition={{duration:0.5, ease:'easeOut', delay:0.3}}>
 
               Directors:
               {
@@ -54,26 +68,34 @@ function Moviepage() {
                   <span key={i}>{' '}{director.displayName}{i !== movie.directors.length - 1 ? ',' : ''}</span>
                 ))
               } 
-            </p>
-            <p>
+            </motion.p>
+            <motion.p
+            initial={{opacity:0, translateY:20}}
+            animate={{opacity:1, translateY:0}}
+            transition={{duration:0.5, ease:'easeOut', delay:0.4}}>
               Writers:
               {
                 movie.writers.map((writer: any, i: number) => (
                   <span key={i}>{' '}{writer.displayName}{i !== movie.writers.length - 1 ? ',' : ''}</span>
                 ))
               } 
-            </p>
-            <p>
+            </motion.p>
+            <motion.p
+            initial={{opacity:0, translateY:20}}
+            animate={{opacity:1, translateY:0}}
+            transition={{duration:0.5, ease:'easeOut', delay:0.5}}>
               Stars:
               {
                 movie.stars.map((star: any, i: number) => (
                   <span key={i}>{' '}{star.displayName}{i !== movie.stars.length - 1 ? ',' : ''}</span>
                 ))
               } 
-            </p>
+            </motion.p>
           </div>
 
-          <p className='mt-4'>{movie.plot}</p>
+          <motion.p initial={{opacity:0, translateY:20}}
+            animate={{opacity:1, translateY:0}}
+            transition={{duration:0.5, ease:'easeOut', delay:0.6}} className='mt-4'>{movie.plot}</motion.p>
             
         </div>
       </div>
