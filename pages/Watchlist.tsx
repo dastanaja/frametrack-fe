d20671b1c95162240059cd909d7a39f7baaef95d
dastanaja <garnidadastan20@gmail.com>
2025-12-10 23:42:37 +0700

    just add margin

--- pages/Watchlist.tsx
+++ pages/Watchlist.tsx
@@ -33,7 +33,7 @@ function Watchlist() {
     fetchWatchlist()
   }, [])
   return (
-    <div className='max-w-[1200px] mx-auto'>
+    <div className='max-w-[1200px] mx-auto pb-40'>
       <motion.h1 
         className='mt-14 mb-18 font-medium text-left text-6xl' 
         initial={{ opacity: 0, y: 20 }}
